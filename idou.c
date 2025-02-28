09a880588a079a7912a40e4c1630dbdb0bba12be
root <m_himenezumi@yahoo.co.jp>
2025-02-28 23:44:19 +0900

    make push

--- idou.c
+++ idou.c
@@ -30,9 +30,28 @@ void	p(t_list **from, t_list **to)
 {
 	t_list	*temp;
 
-	
-	
-
+	temp = *from;
+	if (*from == (*from)->next)
+		*from = NULL;
+	else
+	{
+		(*from)->next->prev = (*from)->prev;
+		(*from)->prev->next = (*from)->next;
+		*from = (*from)->next;
+	}
+	if (!*to)
+	{
+		temp->prev = temp;
+		temp->next = temp; 
+	}
+	else
+	{
+		temp->prev = (*to)->prev;
+		temp->next = *to;
+		(*to)->prev->next = temp;
+		(*to)->prev = temp;
+	}
+	*to = temp;
 }
 
 /*
